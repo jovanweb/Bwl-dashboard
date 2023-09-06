d89075765945b8ef1acd472f7ca30a5935e2328e
Jovanweb <jovanpoznanovic@gmail.com>
2023-09-06 16:21:21 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <FirstStepPDF v-if="step === 1" @previewData="preview"/>
+    <FinalPDF v-if="step === 2" :dataUser="dataUser"/>
+  </main>
+    
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import html2pdf from 'html2pdf.js';
+import FirstStepPDF from './components/FirstStepPDF.vue';
+import FinalPDF from './components/FinalPDF.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    FirstStepPDF, FinalPDF,
+  },
+  data() {
+    return {
+      dataUser: null,
+      step:1,
+    }
+  },
+  methods: {
+    generatePDF() {
+      const content = this.$refs.pdfContent;
+      const pdfOptions = {
+        margin: 10,
+        filename: 'example.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
+      };
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+      html2pdf().from(content).set(pdfOptions).save();
+    },
+
+    preview(preview) {
+      this.dataUser = preview;
+      this.step = 2;
+    }
+  },
 }
+
+</script>
+<style lang="scss">
+
+  html, body, #app {
+    height: 100%;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  p, .form-check-label {
+    color: #555555;
+  }
+</style>
+
+<style lang="scss" scoped>
+
+  main {
+    height: 100%;
+  }
+  #pdf-content {
+    font-family: Arial, sans-serif;
+    background-color: #f0f0f0;
+    padding: 20px;
+  }
+
+  .title {
+    font-size: 24px;
+    color: #333;
+  }
+
+  .content {
+    font-size: 16px;
+    color: #555;
+  }
 </style>
